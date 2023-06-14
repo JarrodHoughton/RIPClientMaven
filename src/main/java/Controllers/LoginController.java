4f33628d068efbe2b5774b4d9d75d36891e16437
JarrodHoughton <136487417+JarrodHoughton@users.noreply.github.com>
2023-06-14 13:29:28 +0200

    Replaced Client Controller With LoginController

--- src/main/java/Controllers/LoginController.java
+++ src/main/java/Controllers/LoginController.java
@@ -4,6 +4,12 @@
  */
 package Controllers;
 
+import Models.Reader;
+import ServiceLayers.LoginService_Impl;
+import ServiceLayers.LoginService_Interface;
+import ServiceLayers.ReaderService_Impl;
+import ServiceLayers.ReaderService_Interface;
+import Utils.PasswordEncryptor;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -18,19 +24,55 @@ import java.util.HashMap;
  */
 @WebServlet(name = "LoginController", urlPatterns = {"/LoginController"})
 public class LoginController extends HttpServlet {
+    private LoginService_Interface loginService;
+    private ReaderService_Interface readerService;
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    public LoginController() {
+        loginService = new LoginService_Impl();
+        readerService = new ReaderService_Impl();
+    }
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        switch (request.getParameter("submit")) {
+            case "login":
+                Reader user = new Reader();
+                user.setEmail(request.getParameter("email"));
+                user.setPasswordHash(request.getParameter("password"));
+                HashMap<String, String> details = loginService.getUserSalt(user.getEmail());
+                String message = "Login Credentials Incorrect: email or password was not found.";
+                if (Boolean.parseBoolean(details.get("userFound"))) {
+                    user.setSalt(details.get("salt"));
+                    user = loginService.loginReader(user);
+                    if (user != null) {
+                        request.setAttribute("user", user);
+                        message = "Login successful.";
+                    }
+                } else {
+                    message = "user not found.";
+                }
+                request.setAttribute("message", message);
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+                break;
+            case "register":
+                message = "This email already exists.";
+                if (!readerService.userExists(request.getParameter("email"))) {
+                    Reader reader = new Reader();
+                    reader.setEmail(request.getParameter("email"));
+                    reader.setSalt(PasswordEncryptor.generateSalt());
+                    reader.setPasswordHash(PasswordEncryptor.hashPassword(request.getParameter("password"), reader.getSalt()));
+                    reader.setName(request.getParameter("name"));
+                    reader.setSurname(request.getParameter("surname"));
+                    reader.setPhoneNumber(request.getParameter("phoneNumber"));
+                    message = loginService.register(reader);
+                    request.setAttribute("user", loginService.loginReader(reader));
+                }
+                request.setAttribute("message",message);
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+                break;
+            default:
+                throw new AssertionError();
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
