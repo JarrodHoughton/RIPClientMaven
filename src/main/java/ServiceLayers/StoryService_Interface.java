49f9a32dafd712d3edf51cf32d5d1e257d5dae8f
JarrodHoughton <136487417+JarrodHoughton@users.noreply.github.com>
2023-06-15 14:43:05 +0200

    Updated Login Controller and Story Service Layer

--- src/main/java/ServiceLayers/StoryService_Interface.java
+++ src/main/java/ServiceLayers/StoryService_Interface.java
@@ -4,10 +4,20 @@
  */
 package ServiceLayers;
 
+import Models.Story;
+import java.util.List;
+
 /**
  *
  * @author jarro
  */
 public interface StoryService_Interface {
-    
+    public Story getStory(Integer storyId);
+    public List<Story> getAllStories();
+    public List<Story> getStoriesInGenre(Integer genreId);
+    public List<Story> getTopPicks();
+    public List<Story> getRecommendations();
+    public String updateStory(Story story);
+    public String deleteStory(Integer storyId);
+    public String addStory(Story story);
 }
