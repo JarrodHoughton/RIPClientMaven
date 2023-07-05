8b1def0745265a060007aa45a5f2c058667c32ee
KylynnvdM <vandermerwekylynn@gmail.com>
2023-07-06 00:47:54 +0200

    SMSService

--- src/main/java/ServiceLayers/SMSService_Impl.java
+++ src/main/java/ServiceLayers/SMSService_Impl.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 /**
@@ -31,16 +32,16 @@ public class SMSService_Impl implements SMSService_Interface{
     }
 
     @Override
-    public String sendSMS(String phoneNumber) {
-        String sendSMSUri = uri + "sendsms";
+    public String sendSMS(String phoneNumber, String message) {
+        String sendSMSUri = uri + "sendsms/{phoneNumber}/{message}";
         webTarget = client.target(sendSMSUri);
-        response = webTarget.request().get();
+        response = webTarget.request(MediaType.APPLICATION_XML).get();
         
         try {
             if (response.getStatus() == Response.Status.OK.getStatusCode()) {
                 return response.readEntity(String.class);
             } else {
-                System.err.println("Failed to send referral email. Response status: " + response.getStatus());
+                System.err.println("Failed to send SMS. Response status: " + response.getStatus());
                 return "Failed to send SMS";
             }
         } finally {
